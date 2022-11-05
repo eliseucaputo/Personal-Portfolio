5a45f96c5ec5fad086567cee0fc9f90725c304ee
Eliseu Caputo <eliseujunior188@gmail.com>
2022-11-05 18:02:05 -0300

    javascript generation

---
+++ site.js
@@ -0,0 +1,22 @@
+
+function submit() {
+    let name = document.getElementById('name').value
+    let email = document.getElementById('email').value
+    let textarea = document.getElementById('textarea').value
+    console.log(name)
+    console.log(email)
+    console.log(textarea)
+    document.getElementById('name').value = ''
+    document.getElementById('email').value = ''
+    document.getElementById('textarea').value = ''
+
+    if(name.trim() === '') {
+        alert('Please, write your name!')
+    } else if (email.trim() === '') {
+        alert('Please, write your e-mail!')
+    } else if (textarea.trim() === '') {
+        alert('Please, write something!')
+    }
+}
+
+
