d05fdb8547fc0bf2467d8fe7578ac9b59874b27b
Eliseu Caputo <eliseujunior188@gmail.com>
2022-12-11 12:17:04 -0300

    add menu hamburger

--- site.js
+++ site.js
@@ -20,3 +20,14 @@ function submit() {
 }
 
 
+
+
+const btnMobile = document.getElementById('btn-mobile')
+
+function toggleMenu() {
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+
